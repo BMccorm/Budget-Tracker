049c4de2ffee61f61aeb40b73f76f5066d6e37c5
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-09-18 20:01:28 -0400

    updated service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,22 +44,48 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+// self.addEventListener('fetch', (event) => {
+//     if (event.request.url.startsWith(self.location.origin)) {
+//         event.respondWith(
+//             caches.match(event.request).then((cachedResponse) => {
+//                 if (cachedResponse) {
+//                     return cachedResponse;
+//                 }
 
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
+//                 return caches.open(RUNTIME).then((cache) => {
+//                     return fetch(event.request).then((response) => {
+//                         return cache.put(event.request, response.clone()).then(() => {
+//                             return response;
+//                         });
+//                     });
+//                 });
+//             })
+//         );
+//     }
+// });
+
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
                     });
-                });
-            })
+            }).catch(err => console.log(err))
         );
+
+        return;
     }
-});
